397000d49e4ebae38b0ae79e2cc821e32ed1d02b
akkhan2122 <awaisak15869t72@gmail.com>
2023-07-04 23:37:31 -0700

    awais

---
+++ app.js
@@ -0,0 +1,16 @@
+
+var typed = new typed(".text", {
+String: ["pepsi"],
+typeSpeed:100,
+backSpeed:100,
+backDelay:1000,
+looop:trye
+});
+function imgSlider(anything){
+    document.querySelector('.starbucks').src = anything;
+}
+
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle')
+    circle.style.background = color;
+}
---
+++ script.js
@@ -0,0 +1,11 @@
+// const navbar = document.querySelector('.navbar');
+// const content = document.querySelector('.content');
+// const toggleButton = document.createElement('button');
+// toggleButton.innerHTML = '&#9776;';
+
+// toggleButton.addEventListener('click', function() {
+//   navbar.classList.toggle('active');
+//   content.classList.toggle('active');
+// });
+
+// document.body.appendChild(toggleButton);
